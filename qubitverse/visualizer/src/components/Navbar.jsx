5a53094b27e3b6e69822f1665f89f34358fcfffb
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2026-07-01 21:38:15 +0530

    UPDATE: 1. Remove dark theme toggle and add demo version note in Navbar

--- qubitverse/visualizer/src/components/Navbar.jsx
+++ qubitverse/visualizer/src/components/Navbar.jsx
@@ -1,16 +1,6 @@
 import React, { useState, useEffect } from "react";
 
 const Navbar = () => {
-    const [darkTheme, setDarkTheme] = useState(false);
-
-    useEffect(() => {
-        if (darkTheme) {
-            document.documentElement.classList.add("dark");
-        } else {
-            document.documentElement.classList.remove("dark");
-        }
-    }, [darkTheme]);
-
     return (
         <nav className="flex items-center justify-between px-6 py-2 bg-gray-100 dark:bg-gray-800 shadow">
             <h1 className="text-2xl font-bold text-gray-800 dark:text-white" style={{ userSelect: "none" }}>
@@ -30,13 +20,9 @@ const Navbar = () => {
                     <code>QubitVerse</code>
                 </div></h1>
             <div className="flex items-center space-x-5">
-                {/* <button
-                    onClick={() => setDarkTheme((prev) => !prev)}
-                    className="px-4 py-2 bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-gray-200 rounded focus:outline-none"
-                    style={{ userSelect: "none" }}
-                >
-                    {darkTheme ? "Light Mode" : "Dark Mode"}
-                </button> */}
+                <span style={{ fontSize: "0.875rem", color: "rgba(230, 50, 50, 0.8)", userSelect: "none", marginRight: "10px" }}>
+                    NOTE: This is a demo version, you may experience 30-40 seconds delay on first run due to inactivity on the server.
+                </span>
                 <button
                     onClick={() => location.reload()}
                     className="px-4 py-2 bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-gray-200 rounded focus:outline-none"
@@ -45,7 +31,7 @@ const Navbar = () => {
                     Clear Circuit
                 </button>
                 <a
-                    href="https://github.com/QubitStorm-HackStacy/QubitStorm"
+                    href="https://github.com/Dark-CodeX/qubitverse"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="text-gray-800 dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 transition-colors"
